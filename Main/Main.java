3e054a76fc32e5dd2e7f00f9697385a138b79660
farahrguibirg <farahrguibi@gmail.com>
2024-12-24 20:54:56 +0100

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -1,26 +1,4 @@
-/*package main;
 
-import Controller.*;
-import DAO.*;
-import Model.*;
-import View.*;
-
-public class Main {
-    public static void main(String[] args) {
-        MainView mainView = new MainView();
-        EmployeView viewEmployee = mainView.getEmployeeView();
-        HolidayView viewHoliday = mainView.getHolidayPanel();
-
-        HolidayImpl daoHoliday = new HolidayImpl();
-        EmployeImpl daoEmployee = new EmployeImpl();
-
-        HolidayModel modelHoliday = new HolidayModel(daoHoliday, daoEmployee);
-        EmployeModel modelEmployee = new EmployeModel(daoEmployee);
-        
-        new HolidayController(viewHoliday, modelHoliday);
-        new EmployeController(modelEmployee, viewEmployee);
-    }
-}*/
 package main;
 
 import Controller.*;
